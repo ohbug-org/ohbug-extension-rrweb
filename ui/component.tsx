afe98035203c36f87b878ea70069a38b542543d3
chenyueban <jasonchan0527@gmail.com>
2021-04-20 21:52:18 +0800

    feat(ui): 使用新版扩展的创建方式

--- ui/component.tsx
+++ ui/component.tsx
@@ -1,50 +1,46 @@
-import { FC, useRef, useMemo } from 'react'
 import { createExtensionUI } from '@ohbug/core'
-import type { OhbugExtensionUIComponentProps } from '@ohbug/types'
 import useDeepCompareEffect from 'use-deep-compare-effect'
 import rrwebPlayer from 'rrweb-player'
 import 'rrweb-player/dist/style.css'
-
-const Component: FC<OhbugExtensionUIComponentProps> = ({ event }) => {
-  const container = useRef<HTMLDivElement>(null)
-  const component = useRef<rrwebPlayer>(null)
-
-  const events = useMemo(
-    () =>
-      event && event?.metaData?.rrweb
-        ? JSON.parse(JSON.stringify(event.metaData.rrweb))
-        : [],
-    [event]
-  )
-
-  useDeepCompareEffect(() => {
-    // @ts-ignore
-    component.current?.$destroy()
-    // @ts-ignore
-    component.current = new rrwebPlayer({
-      target: container.current!,
-      props: {
-        events,
-      },
-    })
-
-    return () => {
-      // @ts-ignore
-      component.current?.$destroy()
-    }
-  }, [events])
-
-  return event && event?.metaData?.rrweb ? (
-    <div ref={container} />
-  ) : (
-    <div>There is no corresponding rrweb data for the current Event</div>
-  )
-}
 
 export default createExtensionUI({
   name: 'OhbugExtensionUIRrweb',
   key: 'rrweb',
   components: {
-    event: Component,
+    event: (React) => ({ event }) => {
+      const container = React.useRef<HTMLDivElement>(null)
+      const component = React.useRef<rrwebPlayer>(null)
+
+      const events = React.useMemo(
+        () =>
+          event && event?.metaData?.rrweb
+            ? JSON.parse(JSON.stringify(event.metaData.rrweb))
+            : [],
+        [event]
+      )
+
+      useDeepCompareEffect(() => {
+        // @ts-ignore
+        component.current?.$destroy()
+        // @ts-ignore
+        component.current = new rrwebPlayer({
+          target: container.current!,
+          props: {
+            events,
+          },
+        })
+
+        return () => {
+          // @ts-ignore
+          component.current?.$destroy()
+        }
+      }, [events])
+
+      return event && event?.metaData?.rrweb ? (
+        <div ref={container} />
+      ) : (
+        <div>There is no corresponding rrweb data for the current Event</div>
+      )
+    },
   },
 })
